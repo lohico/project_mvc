fbccfbc1c7df10a42ca7f95a8e49d343a50c1abe
George Eremia <lohico@yahoo.com>
2017-03-04 17:18:33 +0200

    am modificat ceva

--- libraries/Premium.php
+++ libraries/Premium.php
@@ -1,13 +1,15 @@
 <?php
 class Premium extends User{
  
+
+
 public function __construct(){
     parent::__construct();
+    
   }
   
 
-public function register($data){
-    parent::register($data);
+
 
 
 
